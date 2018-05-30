8ae8a1e9f9cf24cfe982f95efd6e67323a16100c
Алексей <venderser@gmail.com>
2018-05-30 19:53:27 +0300

    New project start

---
+++ src/leson2_tasks/task2.java
@@ -0,0 +1,9 @@
+package leson2_tasks;
+
+public class task2 {
+        public static void main(String[] args) {
+            int a=10;
+            int b =23;
+            System.out.println(a+b);
+        }
+}
