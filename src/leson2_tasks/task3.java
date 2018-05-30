f790dab457ca77b2ec221485caca2e78c123de41
Алексей <venderser@gmail.com>
2018-05-30 21:01:33 +0300

    L2-T3

---
+++ src/leson2_tasks/task3.java
@@ -0,0 +1,35 @@
+package leson2_tasks;
+
+public class task3 {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 6;
+        int sumNumbers = numbersSumm(number1, number2);
+        int umnojenie = proizved(number1, number2);
+        int sumMetods = nmetodsSumm(sumNumbers, umnojenie);
+        String txt = "Java Forever";
+        javaPrint(txt);
+        System.out.println(sumNumbers);
+        System.out.println(umnojenie);
+        System.out.println(sumMetods);
+    }
+
+    static private void javaPrint(String text) {
+        System.out.println(text);
+    }
+
+    static private int numbersSumm(int number1, int number2) {
+        int a = number1 + number2;
+        return a;
+    }
+
+    static private int proizved(int number1, int number2) {
+        int a = number1 * number2;
+        return a;
+    }
+
+    static private int nmetodsSumm(int number1, int number2) {
+        int a = number1 + number2;
+        return a;
+    }
+}
