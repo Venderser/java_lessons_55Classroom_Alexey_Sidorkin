d5049f3d3ecf629c1b2bde8f6cf73f3ac0d23597
Алексей <venderser@gmail.com>
2018-05-31 17:51:11 +0300

    Home work Lesson2

---
+++ src/leson2_tasks/HomeWork.java
@@ -0,0 +1,36 @@
+package leson2_tasks;
+
+/*1. Создайте три переменные, которые содержат любые целые числа. Создайте метод, который вычисляет среднее арифметическое трех целых чисел.
+ Выведите результат вычисления на экран в основном теле программы.
+2. Создайте метод, который выводит на экран фразу "I can do it!". Выведите результат на экран в основном теле программы.
+3. Создайте две, которые содержат любые целые числа. Создайте метод, который вычисляет выражение вида x*y+x+y с использованием операторов с присваиванием.
+Выведите результат вычисления на экран в основном теле программы.
+
+*/
+public class HomeWork {
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 6;
+        int number3 = 7;
+        int averageNumbers = average(number1, number2, number3);
+        int square = squareRoot(number1, number2);
+        javaPrint();
+        System.out.println(averageNumbers);
+        System.out.println(square);
+    }
+
+    static private void javaPrint() {
+        System.out.println("I can do it!");
+    }
+
+
+    static private int average(int number1, int number2, int number3) {
+        int a = (number1 + number2 + number3) / 3;
+        return a;
+    }
+
+    static private int squareRoot(int number1, int number2) {
+        int a = (number1 * number2) + (number1 * number2);
+        return a;
+    }
+}
