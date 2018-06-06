950c01e9f9f12d0e0c0f3ceacd643f7bd1990588
Алексей <venderser@gmail.com>
2018-06-06 18:26:08 +0300

    Homework lesson 4

---
+++ src/com/gmail/venderser/lesson4/homeWork/Seasons.java
@@ -0,0 +1,21 @@
+package com.gmail.venderser.lesson4.homeWork;
+
+public class Seasons {
+    public static void season(int number) {
+        switch (number) {
+            case 1:
+                System.out.println("Winter");
+                break;
+            case 2:
+                System.out.println("Spring");
+                break;
+            case 3:
+                System.out.println("Summer");
+                break;
+            case 4:
+                System.out.println("Autumn");
+                break;
+        }
+    }
+}
+
--- src/com/gmail/venderser/lesson4/homeWork/Task2.java
+++ src/com/gmail/venderser/lesson4/homeWork/Task2.java
@@ -1,14 +1,11 @@
 package com.gmail.venderser.lesson4.homeWork;
 
-public class Task2 { public static void main(String[] args) {
+public class Task2 {
+    public static void main(String[] args) {
 
-    int year = 1232;
-    if (year % 400 == 0) {
-        System.out.println("Leap year");
-    }else {
-        if (year%4 ==0 & year%100 !=0){System.out.println("Leap year");
-        }else {System.out.println("Not a leap year");
-        }
+        Year.year(1232);
     }
+
 }
-}
+
+
